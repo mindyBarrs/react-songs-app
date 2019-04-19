3b27231b8de885a2c2cdfc854e9b37c89794cdca
Min <barrs.mindy@gmail.com>
2019-04-19 14:47:36 -0400

    Added the reducer code

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+
+// STATIC ARRAY OF SONGS
+const songReducer = () => {
+    return [
+        {
+            title: 'No Scrubs',
+            band: 'TLC',
+            duration: '4:05'
+        },
+        {
+            title: 'Save Tonight',
+            band: 'Eagle-Eye Cherry ',
+            duration: '3:56'
+        },
+        {
+            title: 'Maria Maria',
+            band: 'Sanata feat. The Product G&B',
+            duration: '4:22'
+        },
+        {
+            title: 'Pompeii',
+            band: 'Bastille',
+            duration: '3:34'
+        }
+    ]
+};
+
+const SelectedSongReducer = (selectdSong = null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+
+    return selectdSong;
+}
